1be089083820eacf4891548b49d8ddc456e71776
Artem Sabourov <vs.tema@gmail.com>
2025-03-05 23:00:11 +0900

    Changed BufExplorer default behaviour (no VSplit).

--- lua/plugins/bufexplorer.lua
+++ lua/plugins/bufexplorer.lua
@@ -2,7 +2,7 @@ return {
     'jlanzarotta/bufexplorer',
     name = 'bufexplorer',
     config = function()
-        vim.keymap.set('n', '<C-b>', ':BufExplorerHorizontalSplit<CR>', { noremap = true, silent = true })
+        vim.keymap.set('n', '<C-b>', ':BufExplorer<CR>', { noremap = true, silent = true })
         vim.g.bufExplorerDefaultHelp = 0 -- do not show default help
         vim.g.bufExplorerSplitBelow  = 1 -- split below current window
     end
