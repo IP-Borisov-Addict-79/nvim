fd07c3cba333b1ae377ed1fd7ba9077c9b11b09e
Artem Sabourov <vs.tema@gmail.com>
2026-05-21 19:56:16 +0900

    `mini.icons` dependency is ON by default!

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -40,7 +40,7 @@ return {
         },
     },
     -- Optional dependencies
-    -- dependencies = { { "nvim-mini/mini.icons", opts = {} } }, -- uncomment for nvim >= 10
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } }, -- comment out for nvim <= 0.10
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
