b1f398b81c43acdf806b3a11e2a4847351ea33c5
Artem Sabourov <vs.tema@gmail.com>
2026-05-26 21:10:30 +0900

    Change VimWiki dir to ~/Documents/VimWiki

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -1,7 +1,7 @@
 return {
     'vimwiki/vimwiki',
     init = function()
-        vim.g.vimwiki_path = '~/Documents/vimwiki/'
+        vim.g.vimwiki_path = '~/Documents/VimWiki/'
         vim.g.vimwiki_syntax = 'markdown'
         vim.g.vimwiki_ext = 'md'
     end,
